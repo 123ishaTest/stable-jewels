7494fc56fb2ec62d19f2b47e6030ca2c9ae3f9ae
Isha <ishadijcks@gmail.com>
2021-03-31 12:09:14 +0200

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div class="flex flex-col h-screen">
       <sj-header></sj-header>
       <sj-action-generator class="flex-grow"></sj-action-generator>
+      <sj-footer></sj-footer>
     </div>
 
   </div>
@@ -15,9 +16,11 @@ import {App} from "@/App.ts"
 import IgtNotifications from "@/components/util/igt-notifications";
 import SjActionGenerator from "@/components/features/action-generator/sj-action-generator";
 import SjHeader from "@/components/sj-header";
+import SjFooter from "@/components/sj-footer";
 
 export default {
   components: {
+    SjFooter,
     SjHeader,
     SjActionGenerator,
     IgtNotifications,
---
+++ src/components/sj-footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-full h-32 bg-gray-500 shadow-xl flex flex-col items-center justify-around text-white">
+    <p>123IshaTest Games</p>
+    <p>Made with the Incremental Game Template</p>
+    <div class="flex flex-row space-x-4">
+      <span>
+        <a href="https://discord.gg/AvNpq6Ng6S" target="_blank">
+          <img class="w-8 h-8" :src="require('@/assets/socials/discord.png')">
+        </a>
+      </span>
+      <span>
+        <a href="https://github.com/123ishaTest/incremental-game-template" target="_blank">
+          <img class="w-8 h-8" :src="require('@/assets/socials/github.png')">
+        </a>
+      </span>
+      <span>
+        <a href="https://123ishatest.github.io/incremental-game-template-website/" target="_blank">
+          <img class="w-8 h-8" :src="require('@/assets/socials/docusaurus.svg')">
+        </a>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sj-footer",
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
