e374935344b6f942c5962e5ee55fa98f1167b476
Isha Dijcks <ishadijcks@gmail.com>
2025-05-08 21:17:31 +0200

    Add CNAME to build script

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ npm run build
 
 # navigate into the build output directory
 cd dist
+echo "stable-jewels.lands-unknown.com" > CNAME
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
